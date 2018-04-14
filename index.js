3b61f388b56117dd069c9a6253f981b93f518885
firejump <firejump@github.com>
2018-04-14 15:56:35 +0200

    matchmaking

--- index.js
+++ index.js
@@ -3,14 +3,42 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+var nextUserId = 1;
+var socketsByUserId = {}
+var matchedUserId = {}
+var userWaitingForMatch = null;
+
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  var thisUserId = nextUserId++;
+  socketsByUserId[thisUserId] = socket;
+  if (userWaitingForMatch) {
+    matchedUserId[userWaitingForMatch] = thisUserId;
+    matchedUserId[thisUserId] = userWaitingForMatch;
+
+    socketsByUserId[thisUserId].emit('control', 'userMatched');
+    socketsByUserId[userWaitingForMatch].emit('control', 'userMatched');
+
+    userWaitingForMatch = null;
+  } else {
+    userWaitingForMatch = thisUserId;
+  }
+
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    if (matchedUserId[thisUserId]) {
+      socketsByUserId[matchedUserId[thisUserId]].emit('chat message', msg);
+      socketsByUserId[thisUserId].emit('chat message', msg);
+    } else {
+      socketsByUserId[thisUserId].emit('control', 'noMatchYet');
+    }
   });
+
+
+
 });
 
 http.listen(port, function(){
